78698e398d3d849018b27f2e5ad3aeec3c11b087
Tilak Neema <tilakneema11@gmail.com>
2024-10-27 01:07:57 +0530

    vercel

--- website1/website1/settings.py
+++ website1/website1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+39_pc0$=5+4t7z0lzy2$km5200($&jwlado-5^dg6gt!x!+38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['vercel.app','127.0.0.1']
 
 
 # Application definition
