3fa98527eb88fdd178ae0a9f0e94f8bfb20e3daf
Tilak Neema <tilakneema11@gmail.com>
2025-04-27 15:16:44 +0530

    fixed order button

--- website1/urls.py
+++ website1/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('order.html', TemplateView.as_view(template_name='order.html'), name='order_html'),
+    # Remove this line to avoid /order.html route collision
+    # path('order.html', TemplateView.as_view(template_name='order.html'), name='order_html'),
     path('', include('myapp.urls')),
 ]
